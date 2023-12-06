2e145dcb21638dfaba42c89fd99342636f549f8c
merunhowlader <merunhowlader@gamil.com>
2023-12-06 16:05:33 +0600

    about page beginning

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# Next.Js Website Tutorial: Create a Stunning Portfolio Website with Nextjs, Tailwind CSS and Framer-motion🌟
-
-This repository contains starter code for Portfolio website created using NextJs. <br />
-
-For Demo and Final Code checkout following link👇: <br />
-[Nextjs Portfolio Website](https://devdreaming.com//videos/nextjs-tutorial-build-portfolio-tailwind-css-framer-motion#code-links) <br />
-
-If you want to learn how to create it please follow below tutorial👇: <br />
-https://youtu.be/Yw7yWHigGKI <br />
-[![YouTube Video Views](https://img.shields.io/youtube/views/Yw7yWHigGKI?style=social)](https://youtu.be/Yw7yWHigGKI)<br />
-
 ### Images of The Portfolio Website:
 
 ### Resources Used in This Project
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import profilePic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "../components/Icons";
-
+import HireMe from "@/components/HireMe";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 export const index = () => {
   return <div>index</div>;
 };
@@ -48,7 +49,7 @@ export default function Home() {
                   Resume <LinkArrow className={"w-6 ml-1"} />
                 </Link>
                 <Link
-                  href="maito@merunhowlader@gmail.com"
+                  href="mailto:merunhowlader@gmail.com"
                   target={"_blank"}
                   className="ml-4 text-lg font-medium capitalize text-dark underline"
                 >
@@ -58,6 +59,14 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image
+            src={lightBulb}
+            alt="merun kanti howlader"
+            className="w-full h-auto"
+          />
+        </div>
       </main>
     </>
   );
